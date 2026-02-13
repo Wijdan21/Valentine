3b8fe7ff18c012e34b1946ca230aa15f0a4792c1
Wijdan <149307676+Wijdan21@users.noreply.github.com>
2026-02-13 20:55:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const message =`Hii, happy valentine nellaa.i just want to say that and give you some sweet.\noh yeah this has been planned for a long time but i was a little afraid to give my plan because of what you said at that time.\n\nSo yeah that's all I want to say and give to you, May today you feel appreciated, loved, and remembered—because you are that important.
 
-\nWhatever happens in the future, I just hope you're always happy. And if one day you need someone to share your story with, I'll still be there.\n\nAnyway, thanku yeah for everything.`;
+\nWhatever happens in the future, I just hope you're always happy. And if one day you need someone to share your story with, I'll still be there.\n yeah i've heard a lot from all pov and i feel guilty and yeah if there is another chance i will try to be the best for you\n I hope that opportunity really exists. \n\nAnyway, thanku yeah for everything.`;
 
 
 
